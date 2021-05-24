a473d79a828aa3e1c91477ead2483de2b44f9bbb
Jeareth Venegas <venegasjea@gmail.com>
2021-05-23 23:33:46 -0600

    arreglo nombres de variables mal escritas

--- js/script.js
+++ js/script.js
@@ -1,60 +1,62 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-//círculo rojo
-let xPosicionCirculo = 100;
+//círculo 
+let xPosicionCirculo = 250;
 let yPosicionCirculo = 100;
-let xVelocidadCirculo = getRandomArbitrary(-10, 10);
-let xVelocidadCirculo = getRandomArbitrary(-10, 10);
-let radio = 50;
+let xVelocidadCirculo = 5;
+let yVelocidadCirculo = 7;
+let radio = 100;
 
 //cuadrado azul
-let xPosicionSquare = 200;
-let yPosicionSquare = 200;
-let xVelocidadSquare = getRandomArbitrary(-10, 10);
-let xVelocidadSquare = getRandomArbitrary(-10, 10);
-let squa = 50;
+let xPosicionSquare = 250;
+let yPosicionSquare = 100;
+let xVelocidadSquare = 4;
+let yVelocidadSquare = 6;
+let squa = 150;
 
 //animación
 function createDraw(){ 
-    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    //cí rculo
+    if(xPosicionCirculo > canvas.width - radio || xPosicionCirculo < radio){
+        xVelocidadCirculo =- xVelocidadCirculo;
+    }
+
+    if(yPosicionCirculo > canvas.height - radio || yPosicionCirculo < radio){
+        yVelocidadCirculo =- yVelocidadCirculo;
+    }
+    
+    xPosicionCirculo += xVelocidadCirculo;
+    yPosicionCirculo += yVelocidadCirculo;
+
+    //círculo
     ctx.beginPath();
-    ctx.fillStyle = 'red';
     ctx.arc(xPosicionCirculo, yPosicionCirculo, radio, 0, 2 * Math.PI);
+    ctx.fillStyle = 'red';
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
-    
-    if(xPosicionCirculo + yVelocidadCirculo > canvas.width - radio || xPosicionCirculo + yVelocidadCirculo < radio){
-        xVelocidadCirculo =- xVelocidadCirculo;
-    }
-    if(yPosicionCirculo + yVelocidadCirculo > canvas.height - radio || xPosicionCirculo + yVelocidadCirculo < radio){
-        yVelocidadCirculo =- yVelocidadCirculo;
-    }
-
-    xPosicionCirculo += xVelocidadCirculo;
-    yPosicionCirculo += yVelocidadCirculo;
 
     //cuadrado
 
     ctx.fillStyle="blue";
-    ctx.fillRect(xPosicionSquare, yPosicionSquare, squa,squa);
+    ctx.fillRect(75, 75, 150, 150);
 
 
     if(xPosicionSquare + xVelocidadSquare > canvas.width - squa || xPosicionSquare + xVelocidadSquare < squa ) {
         xVelocidadSquare =- xVelocidadSquare;
     }
     if(yPosicionSquare + yVelocidadSquare > canvas.height - squa || yPosicionSquare + yVelocidadSquare < canvas.height - squa){
-        vVelocidadSquare =- vVelocidadSquare;;
+        yVelocidadSquare =- yVelocidadSquare;;
     }
     xPosicionSquare += xVelocidadSquare;
     yPosicionSquare += yVelocidadSquare;
 
     window.requestAnimationFrame(createDraw);
 }
-//createDraw();
+/*createDraw();
+setInterval(createDraw, 10);*/
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
